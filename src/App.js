3b2987a66af998010c712b5a21e733c011e03f78
gabriela cruz <gabriela.cruz18@tectijuana.edu.mx>
2021-10-22 14:33:10 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Swal from 'sweetalert2';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hola.
-        </p>
-      </header>
+      <Header>
+
+      </Header>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Header.css'
+import logo from '../cinelo.jpg'
+
+class Header extends Component{
+    render (){
+        const {titulo, numero}=this.props;
+        return(
+            <div className="Header">
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+        );
+    }
+}
+
+export default Header;
